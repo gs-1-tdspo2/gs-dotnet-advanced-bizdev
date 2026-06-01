ffd7bf8e6395f5b75453d1b222b7264223440539
Sabelli <sabellinho@gmail.com>
2026-06-01 18:33:51 -0300

    Add project files.

---
+++ Amanaje_API/Data/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Amanaje_API.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+    }
+}
